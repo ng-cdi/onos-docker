d5a6c3fb15f81a6e7a793213fd2901e6f5472ca8
Will Fantom <willfantom@gmail.com>
2020-11-10 17:52:51 +0000

    Build the CLI image
    Signed-off-by: Will Fantom <willfantom@gmail.com>

--- build-all.sh
+++ build-all.sh
@@ -3,11 +3,10 @@ set -e
 
 ## Helper script to build all images in order
 
-cd ./Dockerfiles
-
-docker build --rm -f Dockerfile.environment -t ghcr.io/ng-cdi/onos-docker:intents-environment .
-docker build --rm -f Dockerfile.onos -t ghcr.io/ng-cdi/onos-docker:intents-onos .
-docker build --rm -f Dockerfile.imr -t ghcr.io/ng-cdi/onos-docker:intents-app .
-docker build --rm -f Dockerfile.demo -t ghcr.io/ng-cdi/onos-docker:intents-demo .
+docker build --rm -f Dockerfiles/Dockerfile.environment -t ghcr.io/ng-cdi/onos-docker:intents-environment .
+docker build --rm -f Dockerfiles/Dockerfile.onos -t ghcr.io/ng-cdi/onos-docker:intents-onos .
+docker build --rm -f Dockerfiles/Dockerfile.imr -t ghcr.io/ng-cdi/onos-docker:intents-app .
+docker build --rm -f Dockerfiles/Dockerfile.demo -t ghcr.io/ng-cdi/onos-docker:intents-demo .
+docker build --rm -f Dockerfiles/Dockerfile.cli -t ghcr.io/ng-cdi/onos-docker:intents-cli .
 
 echo "Building Completed"
